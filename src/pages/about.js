464e5ced8bd5347bcc45a35047f9d48e207a60db
Akrit Ujjainiya <aakritujjainiya@gmail.com>
2025-05-31 15:34:22 +0530

    Initial Commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,7 +17,7 @@ const about = () => {
         <title>Akrit Ujjainiya | About</title>
         <meta name='description' content='any description' />
       </Head>
-      <TransitionEffect/>
+      <TransitionEffect />
       <main className='flex w-full flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText text="Passion Fuels Purpose!" className=' mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
@@ -37,14 +37,18 @@ const about = () => {
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light dark:bg-dark dark:border-light p-8 xl:col-span-4 md:order-1 md:col-span-6 '>
               <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
-              <Image src={profilePic} alt='' className='w-full h-auto rounded-2xl priority
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"' />
+              <Image
+                src={profilePic}
+                alt=''
+                className='w-full h-auto rounded-2xl'
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
             </div>
-
           </div>
           <Skills />
           <Experience />
-          <Education/>
+          <Education />
         </Layout>
       </main>
     </>
